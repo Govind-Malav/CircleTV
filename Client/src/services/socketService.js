479a6c8499f1a74f02c47196cbbd8d587e0ff618
Govind <govindmalav2004@gmail.com>
2026-02-20 21:48:20 +0530

    socketService.js is added

---
+++ Client/src/services/socketService.js
@@ -0,0 +1,30 @@
+import { io } from 'socket.io-client';
+
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5001';
+
+class SocketService {
+  constructor() {
+    this.socket = null;
+  }
+
+  connect(token) {
+    this.socket = io(SOCKET_URL, {
+      auth: { token }
+    });
+    return this.socket;
+  }
+
+  disconnect() {
+    if (this.socket) this.socket.disconnect();
+  }
+
+  emit(event, data) {
+    this.socket.emit(event, data);
+  }
+
+  on(event, callback) {
+    this.socket.on(event, callback);
+  }
+}
+
+export default new SocketService();
