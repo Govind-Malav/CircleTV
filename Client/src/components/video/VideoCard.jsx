d16abcfd5e2f3aabb8df9a93126c2e6de94d840f
Govind <govindmalav2004@gmail.com>
2026-02-24 21:48:39 +0530

    handleclick function is added

--- Client/src/components/video/VideoCard.jsx
+++ Client/src/components/video/VideoCard.jsx
@@ -7,8 +7,12 @@ import { addToHistory } from '../../store/videoSlice';
 const VideoCard = ({ video }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
+
   const handleClick = () => {
     dispatch(addToHistory(video));
     navigate(`/watch/${video._id}`);
   };
+
+}
+
+export default VideoCard;
