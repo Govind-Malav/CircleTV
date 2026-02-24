04f8c3fcba1b784ef0599b86262012e0a53a5142
Govind <govindmalav2004@gmail.com>
2026-02-24 21:52:44 +0530

    video grid card done

--- Client/src/components/video/VideoGrid.jsx
+++ Client/src/components/video/VideoGrid.jsx
@@ -28,13 +28,13 @@ const VideoGrid = ({ videos, loading, error }) => {
     );
   }
   
-//   return (
-//     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-//       {videos.map(video => (
-//         <VideoCard key={video._id} video={video} />
-//       ))}
-//     </div>
-//   );
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      {videos.map(video => (
+        <VideoCard key={video._id} video={video} />
+      ))}
+    </div>
+  );
 };
 
 export default VideoGrid;
